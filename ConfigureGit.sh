f4488b506b9d1e88e4faeb81e3f0c23a769f27f4
Kevin Armenta <kjarment@calpoly.edu>
2014-04-03 15:01:50 -0700

    track Config script and pledge

---
+++ ConfigureGit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+git config --global user.name "Kevin Armenta"
+git config --global user.email "kjarment@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
